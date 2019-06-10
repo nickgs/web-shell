4638433770fe796c63392d0299ff1fd5a81fdaa9
nickgs <nick@segosolutions.com>
2019-06-10 02:18:59 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+To run the server you'll need to clone this repository and run `npm i && ./node_modules/.bin/tsc . && node app.js`. If that last stepped worked then visiting localhost on port 3000 will present you with a shell that you can interact with as you would with a regular shell.
+
+See the following post for motivation: https://dev.to/davidk01/ptyjs--xtermjs--shell-in-your-browser-1f9c.
+![demo.gif](demo.gif)
---
+++ app.ts
@@ -0,0 +1,30 @@
+import * as express from 'express';
+import * as pty from 'pty.js';
+
+const app = express();
+const expressWs = require('express-ws')(app);
+
+// Serve static assets from ./static
+app.use(express.static(`${__dirname}/static`));
+
+// Instantiate shell and set up data handlers
+expressWs.app.ws('/shell', (ws, req) => {
+  // Spawn the shell
+  // Compliments of http://krasimirtsonev.com/blog/article/meet-evala-your-terminal-in-the-browser-extension
+  const shell = pty.spawn('/bin/bash', [], {
+    name: 'xterm-color',
+    cwd: '/app',
+    env: process.env
+  });
+  // For all shell data send it to the websocket
+  shell.on('data', (data) => {
+    ws.send(data);
+  });
+  // For all websocket data send it to the shell
+  ws.on('message', (msg) => {
+    shell.write(msg);
+  });
+});
+
+// Start the application
+app.listen(4000);
